3a89ee40cafb4d8931928a970ecb063291f89e26
Polina Lazebnikova <54927872+polinalazebnikova@users.noreply.github.com>
2021-05-06 00:45:29 +0300

    Add files via upload

---
+++ PASSPORT_YANDEX/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+const path = require('path');
+const session = require('express-session');
+
+const passport = require('passport');
+const YandexStrategy = require('passport-yandex').Strategy;
+
+app.use(session({ secret: "supersecret", resave: true, saveUninitialized: true }));
+
+let Users = [{'login': 'admin', 'email':'Aks-fam@yandex.ru'},
+            {'login': 'local_js_god', 'email':'ilia-gossoudarev@yandex.ru'},
+            {'login': 'polinln', 'email':'polya.laz59@gmail.com'}];
+
+const findUserByLogin = (login) => {
+    return Users.find((element)=> {
+        return element.login == login;
+    })
+}
+
+const findUserByEmail = (email) => {
+    return Users.find((element)=> {
+        return element.email.toLowerCase() == email.toLowerCase();
+    })
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.login);
+  });
+  //user - объект, который Passport создает в req.user
+passport.deserializeUser((login, done) => {
+    user = findUserByLogin(login);
+        done(null, user);
+});
+
+passport.use(new YandexStrategy({
+    clientID: '0cf43312268b4c29bad7b586c0a7fda3',
+    clientSecret: '83e7f1c1f58b4854bab899c0f6d1ab6c',
+    callbackURL: "http://localhost:8081/auth/yandex/callback"
+  },
+  (accessToken, refreshToken, profile, done) => {
+    let user = findUserByEmail(profile.emails[0].value);
+    user.profile = profile;
+    if (user) return done(null, user);
+
+    done(true, null);
+  }
+));
+
+const isAuth = (req, res, next)=> {
+    if (req.isAuthenticated()) return next();
+
+    res.redirect('/sorry');
+}
+
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+app.get('/sorry', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'sorry.html'));
+});
+app.get('/auth/yandex', passport.authenticate('yandex'));
+
+app.get('/auth/yandex/callback', passport.authenticate('yandex', { failureRedirect: '/sorry', successRedirect: '/private' }));
+
+app.get('/private', isAuth, (req, res)=>{
+    res.send(req.user);
+});
+
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
